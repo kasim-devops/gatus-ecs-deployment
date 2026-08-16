c5c3b6239b87497733a303a03af249f62166474a
akhtarkasim3-debug <akhtarkasim3@gmail.com>
2026-08-16 15:41:14 +0100

    Configured backend and provider

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,16 +1,11 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
-    }
+  required_version = ">= 1.11"
 
-backend "s3" {
-    bucket = "gatus-deployment-bucket"
-    key = "gatus-ecs/terraform.tfstate"
-    region = "eu-west-2"
-    encrypt = true
+ backend "s3" {
+    bucket       = "gatus-deployment-bucket"
+    key          = "gatus-ecs/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
     use_lockfile = true
-}
+  }
 }
