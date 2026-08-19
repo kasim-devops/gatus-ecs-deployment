08b08cba0390397d36d0b7ab9b11d68f5d4705f8
akhtarkasim3-debug <akhtarkasim3@gmail.com>
2026-08-19 12:20:03 +0100

    Added security group module: ALB & ECS task SGs

---
+++ infra/modules/security-groups/main.tf
@@ -0,0 +1,57 @@
+# ALB Security Group
+resource "aws_security_group" "alb" {
+  name   = "alb-sg"
+  vpc_id = var.vpc_id
+
+  # Allow HTTP traffic from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTPS traffic from anywhere
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+# ECS Task Security Group
+resource "aws_security_group" "ecs_task" {
+  name   = "gatus-ecs-task-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "gatus-ecs-task-sg"
+  }
+}
